9cbdae846b2d81188929623b2cf716ff25c82d06
IlliaSmolin <illia.smolin@empeek.io>
2019-11-04 23:16:42 +0200

    Added all of the logical operations, and functional decomposition (using simple voids)

--- BinaryOperations/Program.cs
+++ BinaryOperations/Program.cs
@@ -4,39 +4,129 @@ namespace BinaryOperations
 {
     class Program
     {
-        delegate int CorrectLength(int a, int b); 
         static void Main(string[] args)
         {
-            void multiplication() {
-                Console.Write("Enter the first integer number: ");
-                int a = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Enter the second integer number: ");
-                int b = Convert.ToInt32(Console.ReadLine());
-                string aBin = Convert.ToString(a, 2);
-                string bBin = Convert.ToString(b, 2);
-                string zeroStr = "";
+            int a, b, result, aBinLength, bBinLength, resBinLength, equalLength;
+            string zeroStr = "", aBin, bBin, resBin;
 
-                if (aBin.Length < bBin.Length) { //try to transform this into a CorrectLength function
-                    byte diff = Convert.ToByte(bBin.Length - aBin.Length);
-                    for (byte i = 0; i < diff; i++) {
+            void multiplication()
+            {
+                showInstruction("multiplication");
+                input();
+                result = a & b;
+                binsToSameLength();
+                showResult();
+                repeat();
+            }
+
+            void addition()
+            {
+                showInstruction("addition");
+                input();
+                result = a | b;
+                binsToSameLength();
+                showResult();
+                repeat();
+            }
+
+            void XOR()
+            {
+                showInstruction("XOR");
+                input();
+                result = a ^ b;
+                binsToSameLength();
+                showResult();
+                repeat();
+            }
+
+            void leftShift()
+            {
+                showInstruction("leftShift");
+                input();
+                result = a << b;
+                binsToSameLength();
+                showResult();
+                repeat();
+            }
+
+            void rightShift()
+            {
+                showInstruction("rightShift");
+                input();
+                result = a >> b;
+                binsToSameLength();
+                showResult();
+                repeat();
+            }
+
+            void inversion()
+            {
+                showInstruction("inversion");
+                Console.Write("Enter the first integer number (a): ");
+                a = Convert.ToInt32(Console.ReadLine());
+                result = ~a;
+                Console.WriteLine("\na: {0}", Convert.ToString(a, 2));
+                Console.WriteLine("r: {0}", Convert.ToString(result, 2));
+                Console.WriteLine("\nDecimal result is: {0}", result);
+                repeat();
+            }
+
+            void showResult()
+            {
+                Console.WriteLine($"\na: {aBin}");
+                Console.WriteLine($"b: {bBin}");
+                Console.WriteLine("r: {0}", resBin);
+                Console.WriteLine("\nDecimal result is: {0}", result);
+            }
+
+            void input()
+            {
+                Console.Write("Enter the first integer number (a): ");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the second integer number (b): ");
+                b = Convert.ToInt32(Console.ReadLine());
+                aBin = Convert.ToString(a, 2);
+                bBin = Convert.ToString(b, 2);
+            }
+
+            void binsToSameLength()
+            {
+                resBin = Convert.ToString(result, 2);
+                aBinLength = aBin.Length;
+                bBinLength = bBin.Length;
+                equalLength = Math.Max(aBinLength, bBinLength);
+                resBinLength = resBin.Length;
+
+                if (aBinLength < equalLength)
+                {
+                    zeroStr = "";
+                    int diff = equalLength - aBinLength;
+                    for (int i = 0; i < diff; i++)
+                    {
                         zeroStr += "0";
                     }
                     aBin = zeroStr + aBin;
-                } else if (aBin.Length > bBin.Length) {
-                    byte diff = Convert.ToByte(aBin.Length - bBin.Length);
-                    for (byte i = 0; i < diff; i++)
+                }
+                if (bBinLength < equalLength)
+                {
+                    zeroStr = "";
+                    int diff = equalLength - bBinLength;
+                    for (int i = 0; i < diff; i++)
                     {
                         zeroStr += "0";
                     }
                     bBin = zeroStr + bBin;
                 }
-
-                Console.WriteLine($"\na: {aBin}");
-                Console.WriteLine($"b: {bBin}");
-                int result = a & b;
-                Console.WriteLine("r: {0}", Convert.ToString(result, 2));
-                Console.WriteLine("\nLogical multiplication is: {0}", result);
-                repeat();
+                if (resBinLength < equalLength)
+                {
+                    zeroStr = "";
+                    int diff = equalLength - resBinLength;
+                    for (int i = 0; i < diff; i++)
+                    {
+                        zeroStr += "0";
+                    }
+                    resBin = zeroStr + resBin;
+                }
             }
 
             void repeat()
@@ -45,7 +135,7 @@ namespace BinaryOperations
                 if (Console.ReadLine() == "y")
                 {
                     Console.Clear();
-                    multiplication();
+                    menu();
                 }
                 else
                 {
@@ -53,8 +143,89 @@ namespace BinaryOperations
                 }
             }
 
+            void menu()
+            {
+                byte listCounter = 0;
+                Console.WriteLine("What do you want to choose?");
+                Console.WriteLine($"{++listCounter}) Logical multiplication");
+                Console.WriteLine($"{++listCounter}) Logical addition");
+                Console.WriteLine($"{++listCounter}) Logical XOR");
+                Console.WriteLine($"{++listCounter}) Logical inversion");
+                Console.WriteLine($"{++listCounter}) Logical left shift");
+                Console.WriteLine($"{++listCounter}) Logical right shift");
+                Console.WriteLine($"{++listCounter}) Logical exit");
+                Console.Write("\nYour choice: ");
+                string answer = Console.ReadLine();
+                Console.Clear();
+
+                switch (answer)
+                {
+                    case "1":
+                        multiplication();
+                        break;
+                    case "2":
+                        addition();
+                        break;
+                    case "3":
+                        XOR();
+                        break;
+                    case "4":
+                        inversion();
+                        break;
+                    case "5":
+                        leftShift();
+                        break;
+                    case "6":
+                        rightShift();
+                        break;
+                    case "7":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("You did the unexsisting choice.");
+                        repeat();
+                        break;
+                }
+            }
+
+            void showInstruction(string operation)
+            {
+                switch (operation)
+                {
+                    case "multiplication":
+                        Console.WriteLine("Logical multiplication transfers two numbers into binary code and compares two digits with the same column");
+                        Console.WriteLine("If two digits are equal to 1, result will have the 1 in this column");
+                        Console.WriteLine("If at least one of the digits isn't equal to 1, result will have the 0 in this column\n");
+                        break;
+                    case "addition":
+                        Console.WriteLine("Logical addition transfers two numbers into binary code and compares two digits with the same column");
+                        Console.WriteLine("If at least one of the digits is equal to 1, result will have the 1 in this column");
+                        Console.WriteLine("If both of the digits are equal to 0, result will have the 0 in this column\n");
+                        break;
+                    case "XOR":
+                        Console.WriteLine("Logical eXclusive OR (XOR) transfers two numbers into binary code and compares two digits with the same column");
+                        Console.WriteLine("Only if one of the digits is equal to 1, result will have the 1 in this column");
+                        Console.WriteLine("If both of the digits are equal to 0, result will have the 0 in this column");
+                        Console.WriteLine("If both of the digits are equal to 1, result will have the 0 in this column\n");
+                        break;
+                    case "inversion":
+                        Console.WriteLine("Logical inversion transfers the number into binary code and changes each digit's state");
+                        Console.WriteLine("0 becomes 1, and 1 becomes 0\n");
+                        break;
+                    case "leftShift":
+                        Console.WriteLine("Logical left shift transfers the first number into binary code and moves the code left on specified number of moves (b)\n");
+                        break;
+                    case "rightShift":
+                        Console.WriteLine("Logical right shift transfers the first number into binary code and moves the code right on specified number of moves (b)\n");
+                        break;
+                    default:
+                        Console.WriteLine("%UNEXISTING_INSTRUCTION%");
+                        break;
+                }
+            }
+
             Console.WriteLine("Welcome to the \"Binary Operations!\" program!\n");
-            multiplication();
+            menu();
         }
     }
 }
