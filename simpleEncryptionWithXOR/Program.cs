f1ace3cb42895b63d4741769167abe85b5083034
IlliaSmolin <illia.smolin@empeek.io>
2019-11-05 14:02:01 +0200

    Added simple encryption problem solving

---
+++ simpleEncryptionWithXOR/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string eKey, dKey;
+            int encrypt, decrypt;
+
+            Console.Write("Write integer number: ");
+            decrypt = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Write encryption key: ");
+            eKey = Console.ReadLine();
+            encrypt = Convert.ToInt32(decrypt) ^ Convert.ToInt32(eKey);
+            Console.WriteLine($"Encrypted {decrypt} is = {encrypt}");
+            Console.Write("Write decryption key: ");
+            dKey = Console.ReadLine();
+            decrypt = Convert.ToInt32(encrypt) ^ Convert.ToInt32(dKey);
+            Console.WriteLine($"Decrypted {encrypt} is = {decrypt}");
+        }
+    }
+}
